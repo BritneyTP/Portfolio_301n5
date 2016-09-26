92cc47eaadb4791efbaae8883be3a8e27f86f909
Britney <britney.presley26@gmail.com>
2016-09-26 15:47:01 -0700

    html, js, and css

---
+++ article.js
@@ -0,0 +1,12 @@
+var articles = [];
+
+/* Object constructor based on my data*/
+function Article(obj) {
+  this.author = obj.author;
+  this.publishedOn = obj.publishedOn;
+  this.title = obj.title;
+  this.body = obj.body;
+  this.category = obj.category;
+}
+
+/* method for my Article object constructor */
