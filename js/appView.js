0d527f00345114b49c393efe29b9319f72e69e10
Britney <britney.presley26@gmail.com>
2016-10-23 16:57:06 -0700

    cleaned code and made linked appView to my html page.

--- js/appView.js
+++ js/appView.js
@@ -5,32 +5,50 @@ var articleView = {};
 
 
 //Add event handler to fade in the nav menu
-articleView.handleNavMenu = function() {
-  $('nav').on('click', '.nav-tab', function() {
-    $('.menu-content').hide();
-    var $id = $(this).attr('data-content');
-    $('main').find('#' + $id).fadeIn();
-  });
-  $('nav .nav-tab:first').click();
+articleView.handleMainNav = function () {
+ //
+ $('.main-nav').on('click', function() {
+  //  $('.tab-content').hide();
+   $('#' + $(this).data('.tab-content')).fadeIn(500);
+ });
+ $('.main-nav :first').click();
 };
+// articleView.handleNavMenu = function() {
+//   $('nav').on('click', function() {
+//     $('main').find('#' + $id).fadeIn();
+//   });
+//   $('nav: first').click();
+// };
 
 //add event handler to show more content
 articleView.handleShowMore = function() {
-  $('.article-body').hide();
-  $('article').on('click', '.read-more', function() {
-    event.preventDefault();
-    console.log('THIS.PARENT', $(this).parent());
-    $(this).parent().find('.article-body').show();
-    $(this).html('Show less &rarr;').removeClass('read-more').addClass('show-less');
-  });
+   $('#article-body *:nth-of-type(n+2)').hide();
+   $('#articles').on('click', '.read-on', function(e) {
+     e.preventDefault();
+     $(this).parent().find('#article-body > *:nth-of-type(n+2)').toggle(750);
+     if($(this).text().includes('Read')) {
+       $(this).text('Show Less \u2191');
+     } else {
+       $(this).text('Read On \u2192');
 
-  $('article').on('click', '.show-less', function() {
-    event.preventDefault();
-    $(this).parent().find('.article-body').hide();
-    $(this).html('read-more &rarr;').removeClass('show-less').addClass('read-more');
-  });
+     }
+
+   });
+//   $('#article-body').hide();
+//   $('articles').on('click', function() {
+//     // event.preventDefault();
+//     // console.log('THIS.PARENT', $(this).parent());
+//     $(this).parent().find('#article-body').show();
+//     $(this).html('Show less &rarr;').removeClass('read-more').addClass('show-less');
+//   });
+//
+//   $('articles').on('click', '.show-less', function() {
+//     // event.preventDefault();
+//     $(this).parent().find('#article-body').hide();
+//     $(this).html('read-more &rarr;').removeClass('show-less').addClass('read-more');
+//   });
+articleView.handleNavMenu();
+articleView.handleShowMore();
 };
 
 //Call the functions
-articleView.handleNavMenu();
-articleView.handleShowMore();
