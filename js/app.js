16b00bd6b951a9943f5e6ee4427ac03460fcec5d
Britney <britney.presley26@gmail.com>
2016-11-10 14:17:44 -0800

    changes made

--- js/app.js
+++ js/app.js
@@ -12,34 +12,12 @@ function Article(obj) {
 
 //Article object literal
 Article.prototype.toHtml = function() {
-  // var $newArticle = $('article.template').clone();
-  // $newArticle.find('.byline span').text(this.author);
-  // $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  // $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 365 / 1000) + ' years ago');
-  // $newArticle.find('h1').html(this.title);
-  // $newArticle.find('.article-body').html(this.body);
-  // $newArticle.attr('data-category', this.category);
+
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 365 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   //Add handlebars to render the articles
-  var source = $("#article-template").html();
+  var source = $('#article-template').html();
   var template = Handlebars.compile(source);
 
   return template(this);
 };
-
-//Sort the method so that the obj in myData array will be first
-myData.sort(function(a, b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
-
-//Create a new obj w/article obj constructor for each object in myData array
-//push it to the articles array
-myData.forEach(function(obj) {
-  articles.push(new Article(obj));
-});
-
-//Append each article to html as a new <article> in #articles sect.
-articles.forEach(function(a) {
-  $('#articles').append(a.toHtml());
-});
