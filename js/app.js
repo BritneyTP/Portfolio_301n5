e3b83ff4192d9d14d0e1100492a1819ee0daf46e
Britney <britney.presley26@gmail.com>
2016-11-13 11:06:33 -0800

    added json file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// Articles array that loadAll is pushing into
 var articles = [];
 
 /* Object constructor based on my data*/
@@ -14,14 +14,40 @@ function Article(obj) {
 //Article object literal
 Article.prototype.toHtml = function() {
 
-  // this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 365 / 1000);
-  // this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 365 / 1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   //Add handlebars to render the articles
-  var source = $('#article-template').html();
+  var source = $("#article-template").html();
   var template = Handlebars.compile(source);
 
   return template(this);
 };
+
+Article.fetchAll = function() {
+  $.ajax({
+    url: 'data/word.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data){
+      console.log(data);
+      Article.loadAll(data)
+    },
+  });
+};
+
+
+Article.loadAll = function(jsonData) {
+  // Sort by data from the json data
+  jsonData.sort(function(a, b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+
+  // Push to the articles array on top
+  jsonData.forEach(function(obj) {
+    articles.push(new Article(obj));
+  });
+}
+
 
 //Sort the method so that the obj in myData array will be first
 myData.sort(function(a, b) {
