69575247fbc2d7b1d6f98b2c8e3c5e9cdb26f8d4
Britney <britney.presley26@gmail.com>
2016-11-19 01:25:21 -0800

    added data

--- js/appView.js
+++ js/appView.js
@@ -1,57 +1,118 @@
-'use strict';
+// 'use strict';
+// (function(module) {
+//   //configure a view obj to hold all functions
+//   var articleView = {};
+//
+//
+//   //Add event handler to fade in the nav menu
+//   articleView.handleMainNav = function () {
+//     //
+//     $('.main-nav').on('click', function() {
+//       //  $('.tab-content').hide();
+//       $('#' + $(this).data('.tab-content')).fadeIn(500);
+//     });
+//     $('.main-nav :first').click();
+//   };
+//
+//   //add event handler to show more content
+//   articleView.handleShowMore = function() {
+//     $('#article-body *:nth-of-type(n+2)').hide();
+//     $('#articles').on('click', '.read-on', function(e) {
+//       e.preventDefault();
+//       $(this).parent().find('#article-body > *:nth-of-type(n+2)').toggle(750);
+//       if($(this).text().includes('Read')) {
+//         $(this).text('Show Less \u2191');
+//       } else {
+//         $(this).text('Read On \u2192');
+//
+//       }
+//
+//     });
+//
+//     $('#').show();
+//     $('#word').val(JSON.stringify(article) + ',');
+//
+//     article.initIndexPage = function(){
+//       Article.all.forEach(function(a){
+//         $('#articles').append(a.html());
+//       });
+//
+//     };
+//     articleView.handleNavMenu();
+//     articleView.handleShowMore();
+//   };
+//
+//   articleView.initAdminPage = function(){
+//     var template = Handlebars.compile($('#author-template').text());
+//
+//     Article.numWordsByAuthor().forEach(function(stat) {
+//       $('.author-stats').append(template(stat));
+//     })
+//
+//     ('#blog-stats .articles').text(Article.all.length);
+//     ('#blog-stats .words').text(Article.numWordsAll());
+//   };
+//   //Call the functions
+//   module.articleView = articleView;
+// })(window);
 (function(module) {
-  //configure a view obj to hold all functions
+  'use strict';
+
   var articleView = {};
+  $(document).ready(function() {
 
+    $('.tab-content').hide();
+    $('#home').fadeIn();
+    $('body').toggleClass('home-body');
 
-  //Add event handler to fade in the nav menu
-  articleView.handleMainNav = function () {
-    //
-    $('.main-nav').on('click', function() {
-      //  $('.tab-content').hide();
-      $('#' + $(this).data('.tab-content')).fadeIn(500);
-    });
-    $('.main-nav :first').click();
-  };
-
-  //add event handler to show more content
-  articleView.handleShowMore = function() {
-    $('#article-body *:nth-of-type(n+2)').hide();
-    $('#articles').on('click', '.read-on', function(e) {
-      e.preventDefault();
-      $(this).parent().find('#article-body > *:nth-of-type(n+2)').toggle(750);
-      if($(this).text().includes('Read')) {
-        $(this).text('Show Less \u2191');
-      } else {
-        $(this).text('Read On \u2192');
-
-      }
-
+    // MOBILE DROPDOWN //
+    $('#header-menu').on('click', function() {
+      $('#main-nav').toggleClass('open-nav');
     });
 
-    $('#').show();
-    $('#word').val(JSON.stringify(article) + ',');
+    // MAIN NAVIGATION //
+    $('#main-nav').on('click', '.tab', function() {
+      var $tab = $(this).attr('data-content');
+      console.log($tab);
+      $('.tab-content').fadeOut();
+      $('#' + $tab).fadeIn();
+    });
 
-    article.initIndexPage = function(){
-      Article.all.forEach(function(a){
-        $('#articles').append(a.html());
-      });
+    $('#home-nav').on('click', function() {
+      $('body').addClass('home-body');
+    });
 
-    };
-    articleView.handleNavMenu();
-    articleView.handleShowMore();
-  };
+    $('#about-nav, #blog-nav, #contact-nav').on('click', function() {
+      $('body').removeClass('home-body');
+    });
 
-  articleView.initAdminPage = function(){
-    var template = Handlebars.compile($('#author-template').text());
+    // RESUME //
+    $('#resume-button').on('click', function() {
+      $('.tab-content').hide();
+      $('#resume').fadeIn();
+      $('body').removeClass('home-body');
+    });
 
-    Article.numWordsByAuthor().forEach(function(stat) {
-      $('.author-stats').append(template(stat));
-    })
+    // PORTFOLIO //
+    $('#portfolio-button').on('click', function() {
+      $('.tab-content').hide();
+      $('#portfolio').fadeIn();
+      $('.show-full').hide();
+      $('body').removeClass('home-body');
+    });
 
-    ('#blog-stats .articles').text(Article.all.length);
-    ('#blog-stats .words').text(Article.numWordsAll());
-  };
-  //Call the functions
+    $('#portfolio .proj-logo').on('mouseover', function() {
+      $('.proj-logo').hide();
+      $('.show-full').fadeIn();
+    });
+
+    $('#portfolio .show-full').on('mouseout', function() {
+      $('.show-full').hide();
+      $('.proj-logo').fadeIn();
+    });
+
+    
+
+  });
   module.articleView = articleView;
 })(window);
